2aeae73f729d7004e617e0b3522afac4110ae142
bn66 <bnguyen525@gmail.com>
2014-12-21 14:19:03 -0500

    thing
    syncing up to

--- eulerproject15.py
+++ eulerproject15.py
@@ -8,15 +8,30 @@ def lattice(size):
     """T
     O(???)
     """
-    a = [(0,0)]
-    pos = set(a)
+    # ordered dictionary?
     
-    
-    
-    print pos
+    # psb = [[(0,0)]]
+    psb = [[(0,0),(1,0)],[(0,0),(0,-1)]]
+    # if it's 1, print 2
+    while True:
+        for i in psb:
+            # print i
+            # print size**2
+            if len(i) < size*2:
+                print "in if"
+                if i[-1][0] < size or i[-1][1] < size:
+                    # print "first", i[-1][0]
+                    # print "second", i[-1][1]
+                    
+                # if # last entry in i is less than the length or width # absolute value
+                # set[0]
+                # elif:
+                    # add 0
+                # i.append(i)
+    print psb
 	
 
 
 
 
-lattice(1)
+lattice(2)
