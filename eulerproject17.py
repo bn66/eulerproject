3aa4b630eb49662801f3e100081443f6352a785b
bn66 <bnguyen525@gmail.com>
2014-12-25 00:34:37 -0500

    17 working

--- eulerproject17.py
+++ eulerproject17.py
@@ -3,15 +3,14 @@ Euler Project Problem #17
 Number letter counts
 http://projecteuler.net/problem=17
 """
-
+import math
 def number_word(n):
-    """
-    
-    Recursive
-    Do not count spaces or hyphens. For example, 342 (three hundred and 
-    forty-two) contains 23 letters and 115 (one hundred and fifteen)
-    contains 20 letters. The use of "and" when writing out numbers is in
-    compliance with British usage.
+    """Recursively calculates the number of letters in spelling out a 
+    number, n, returns the value. Spaces and hyphens are not counted, 
+    and 'and' is used. For example, 342 (three hundred and forty-two) 
+    returns 23 and 115 (one hundred and fifteen) returns 20.The use of 
+    "and" when writing out numbers is in compliance with British usage.
+    O(1)
     """
     # dictionary  two of them one for words one for letters
     value = 0
@@ -33,82 +32,80 @@ def number_word(n):
         10: 3, # ten
         11: 6, # eleven
         12: 6, # twelve
-        # 20f;lkafj;ljjjjjjjjjjjjjjjjjjjjjjjjj
-        20: 4 + 2, # twen + ty
         
-        # roots impure
+        # Weird
+        '20': 4 + 2, # twen + ty
+        '40': 5, # for + ty
+        
+        # Impure roots
         '3': 4, # thir-
         '5': 3, # fif-
         '8': 4, # eigh-
-        '40': 3, # for-
-        
-        
-        # 1000: 3 + 8 # one + thousand 
     }
-    
     # Length of number
     if n == 0:
         a = 0
     else:
-        a = len(str(n))
-        # a = math.floor(math.log10(n))+1 # faster
+        # a = len(str(n))
+        a = math.floor(math.log10(n))+1 # faster
         
     # Number processing
-    if a > 3 and a < 7: # thousands
-        value += 8 # t-h-o-u-s-a-n-d
-        value += nums[n // 1000]
+    if a > 3 and a < 7: # Thousands
+        value += nums[n // 1000] + 8 # t-h-o-u-s-a-n-d
         value += number_word(n % 1000)
         return value
-    elif a == 3: # hundreds
+    elif a == 3: # Hundreds
         value += nums[n // 100] + 7 # h-u-n-d-r-e-d
         if n % 100 != 0:
             value += number_word(n % 100) + 3 # 'and'
-        else:
-            value += 0
         return value
     elif a == 2: # 10 through 99
-        x = n // 10 # tens place
-        y = n % 10 # ones place
+        x = n // 10 # Tens place
+        y = n % 10 # Ones place
         
+        # Hard-coded: 10 11 12
         try:
-            value += nums[n] # hard-coded: 10 11 12 20 **************
+            value += nums[n]
             return value
         except KeyError:
             pass
         
-        if x == 4: # forty and fourteen are different
-            value += nums['40'] + 2 # f-o-r + t-y
+        # Weird
+        if x == 2:
+            value += nums['20'] # t-w-e-n-t-y
+            value += number_word(y)
+            return value       
+        if x == 4: 
+            value += nums['40'] # f-o-r + t-y
             value += number_word(y)
             return value
+        
         if x == 1: # Teens, 13 through 19
             try:
-                value += nums[str(y)] + 4 # impure roots: 3 5 8 + -teen
+                value += nums[str(y)] + 4 # Impure roots: 3 5 8 + -teen
                 return value
             except KeyError:
                 value += nums[y] + 4 # 4 6 7 9 + -teen
-                return value            
-        elif x >= 2: # 21?????????????? through 99
+                return value        
+        elif x >= 3: # 31 through 99
             try:
-                value += nums[str(x)] + 2 # impure roots: 3 5 8 + -ty
+                value += nums[str(x)] + 2 # Impure roots: 3 5 8 + -ty
                 value += number_word(y)
                 return value
             except KeyError:
                 value += nums[x] + 2 # 6 7 9 + -ty
                 value += number_word(y)
                 return value
-
     elif a == 1: # 1 through 9
         value += nums[n]
         return value
     elif a == 0:
         return 0
-    else:
-        print "Error"
-        return None
     return value
 
 def num_let_count(a, b):
-    """Lit
+    """
+    
     """
     sum = 0
     
@@ -119,7 +116,7 @@ def num_let_count(a, b):
         """, sum
         
 # tests
-num_let_count(1, 1000)
+# num_let_count(1, 1000)
 # def number_word(100)
 """
 Notes:
