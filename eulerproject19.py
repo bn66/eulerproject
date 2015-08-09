a3f5aced1398aeae5512db242eb8592db5c76177
bn66 <bnguyen525@gmail.com>
2015-08-09 17:19:26 -0400

    Solved the Problem

--- eulerproject19.py
+++ eulerproject19.py
@@ -15,7 +15,10 @@ def sunday_count(year):
     months = [('Jan',31), ('Feb',28), ('Mar',31), ('Apr',30), 
         ('May',31), ('Jun',30), ('Jul',31), ('Aug',31), 
         ('Sep',30), ('Oct',31), ('Nov',30), ('Dec',31)]
-    if year % 4 == 0 and year % 400 != 0: # Leap year
+        
+    if year % 400 == 0: # Unless divisible by 400
+        months[1] = ('Feb',29)
+    elif year % 4 == 0 and year % 100 != 0: # 4 years, not on century
         months[1] = ('Feb',29)
         # print "leap year"
     
@@ -38,26 +41,9 @@ def sunday_count(year):
         
     return sundays
     
-    
-def first_day(year):
-    """Input is a year number
-    Return first day of given year, 
-    O(???)
-    """
-    # 1 Jan 1900 was a Monday
-    days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']
-    # Moves by 1 day for or 2 for leep years
-    day = 1 # Monday in 1900
-    day += (year - 1900)
-    if year > 1900:
-        day += (year - 1901) // 4        
-    day = day % 7
-    return day
-    # return days[day]
-    
 # generate first dates
 a = 0
-for i in range(1901, 1905):
+for i in range(1901, 2001):
     a += sunday_count(i)
     # a += sunday_count(first_day(i))
     # print i, first_day(i)
